49b7297901ef8bbecd0935262ccbefac57ef8569
AslamHossain <aslamecehstu43java@gmail.com>
2020-10-27 02:47:31 +0600

    creation Docker file

---
+++ Rest_api/src/main/java/com/docker/root/controllers/Home.java
@@ -0,0 +1,14 @@
+package com.docker.root.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+	@RequestMapping(value = "/")
+	public String sayHello() {
+		return "Hello ! I am from docker";
+	}
+
+}
